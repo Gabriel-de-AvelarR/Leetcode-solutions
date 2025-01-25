f19c469ba5a072159fe4f5a69a20c174fe9767e9
Gabriel-de-AvelarR <76533839+Gabriel-de-AvelarR@users.noreply.github.com>
2025-01-24 21:19:42 -0300

    Time: 0 ms, Memory: 11.5 MB - LeetHub

--- 41061-1982-3-longest-substring-without-repeating-characters/41061-1982-3-longest-substring-without-repeating-characters.cpp
+++ 41061-1982-3-longest-substring-without-repeating-characters/41061-1982-3-longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+
+    //ABORDAGEM PARA UMA ESTRUTURA DE DADOS DINAMICA
+
+    int lengthOfLongestSubstringA(string s) {
         unordered_set<char> pastLetters;
         auto windowStart = s.begin();
         int maxString = 0;
@@ -13,6 +16,23 @@ public:
 
             pastLetters.insert(*currentPos);
             maxString = max(maxString, static_cast<int>(currentPos - windowStart + 1));
+        }
+
+        return maxString;
+    }
+
+    //ABORDAGEM PARA UM ARRAY FIXO 
+    int lengthOfLongestSubstring(string s) {
+        vector<int> pastLettersIndex(256, -1); 
+        int maxString = 0;             
+        int windowStart = 0;                  
+
+        for (int currentPos = 0; currentPos < s.size(); currentPos++) {
+            if (pastLettersIndex[s[currentPos]] >= windowStart) 
+                windowStart = pastLettersIndex[s[currentPos]] + 1;
+            
+            pastLettersIndex[s[currentPos]] = currentPos;
+            maxString = max(maxString, currentPos - windowStart + 1);
         }
 
         return maxString;
